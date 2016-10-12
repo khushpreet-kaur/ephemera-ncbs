867e906fe08a2b2bd92d87368ff080164b2c3261
Khushpreet <khushpreetk363@gmail.com>
2016-10-12 20:12:54 +0530

    Fix indentation

--- main.js
+++ main.js
@@ -8,23 +8,23 @@ var Images = Backbone.Collection.extend({
 	model: Image,
 	getRandomImage: function(path) {
 	    path = path || './images/';
-            var idx = Math.floor( Math.random() * this.models.length );
-            return this.models[ idx ];
+        var idx = Math.floor( Math.random() * this.models.length );
+        return this.models[ idx ];
 	}
 });
 
 var EphermeraView = Backbone.View.extend({
 	initialize: function(obj) {
-            this.collection = obj.collection;
+        this.collection = obj.collection;
 	    this.path = './images';
 	}, 
 
 	render: function() {
 	    var img = this.collection.getRandomImage(this.path);
-            var elem = document.createElement("img");
-            elem.src = this.path + "/" + img.get('src');
-            elem.style.width = "50%";
-            document.getElementById('img-container').appendChild(elem);
+        var elem = document.createElement("img");
+        elem.src = this.path + "/" + img.get('src');
+        elem.style.width = "50%";
+        document.getElementById('img-container').appendChild(elem);
 	}
 });
 
