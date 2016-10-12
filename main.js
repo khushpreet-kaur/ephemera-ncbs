cf63890efc9af4965cd0544fd3b96e807c3e8b50
Khushpreet <khushpreet-kaur@users.noreply.github.com>
2016-10-12 20:10:51 +0530

    Correct indentation

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 var Image = Backbone.Model.extend({
 	defaults: {
-		src: ""
+	    src: ""
 	}
 });
 
 var Images = Backbone.Collection.extend({
 	model: Image,
 	getRandomImage: function(path) {
-		path = path || './images/';
-    var idx = Math.floor( Math.random() * this.models.length );
-    return this.models[ idx ];
+	    path = path || './images/';
+            var idx = Math.floor( Math.random() * this.models.length );
+            return this.models[ idx ];
 	}
 });
 
 var EphermeraView = Backbone.View.extend({
 	initialize: function(obj) {
-		this.collection = obj.collection;
-		this.path = './images';
+            this.collection = obj.collection;
+	    this.path = './images';
 	}, 
 
 	render: function() {
-	  var img = this.collection.getRandomImage(this.path);
-    var elem = document.createElement("img");
-    elem.src = this.path + "/" + img.get('src');
-    elem.style.width = "50%";
-    // $("#img-container").append(elem);
-    document.getElementById('img-container').appendChild(elem);
+	    var img = this.collection.getRandomImage(this.path);
+            var elem = document.createElement("img");
+            elem.src = this.path + "/" + img.get('src');
+            elem.style.width = "50%";
+            document.getElementById('img-container').appendChild(elem);
 	}
 });
 
@@ -38,7 +37,5 @@ var images = new Images([
 	{src: "image6.jpg"}
 ]);
 
-
 var myView = new EphermeraView({collection: images});
-
 myView.render();
