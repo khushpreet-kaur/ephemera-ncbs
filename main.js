bb240c684f3d6806d68522864486d9c2c867d667
Khushpreet <khushpreetk363@gmail.com>
2016-10-12 19:57:37 +0530

    Ephemera using Backbone js.
    - To render a random image in every page refresh.

---
+++ main.js
@@ -0,0 +1,44 @@
+var Image = Backbone.Model.extend({
+	defaults: {
+		src: ""
+	}
+});
+
+var Images = Backbone.Collection.extend({
+	model: Image,
+	getRandomImage: function(path) {
+		path = path || './images/';
+    var idx = Math.floor( Math.random() * this.models.length );
+    return this.models[ idx ];
+	}
+});
+
+var EphermeraView = Backbone.View.extend({
+	initialize: function(obj) {
+		this.collection = obj.collection;
+		this.path = './images';
+	}, 
+
+	render: function() {
+	  var img = this.collection.getRandomImage(this.path);
+    var elem = document.createElement("img");
+    elem.src = this.path + "/" + img.get('src');
+    elem.style.width = "50%";
+    // $("#img-container").append(elem);
+    document.getElementById('img-container').appendChild(elem);
+	}
+});
+
+var images = new Images([
+	{src: "image1.jpg"},
+	{src: "image2.jpg"},
+	{src: "image3.jpg"},
+	{src: "image4.jpg"},
+	{src: "image5.jpg"},
+	{src: "image6.jpg"}
+]);
+
+
+var myView = new EphermeraView({collection: images});
+
+myView.render();
